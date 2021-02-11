b25670d8d2665cf515cf06dd6dbd4b438116d8af
Christian Bartolo <christian.bartolo.16@um.edu.mt>
2021-02-11 08:53:42 +0100

    removed test folders

--- examples/src/main/scala/examples/execute/testcoin/CPSPc.scala
+++
@@ -1,6 +0,0 @@
-package examples.execute.testcoin
-
-import lchannels.In
-sealed abstract class ExternalChoice1
-case class Heads()(val cont: In[ExternalChoice1]) extends ExternalChoice1
-case class Tails()(val cont: In[ExternalChoice1]) extends ExternalChoice1
