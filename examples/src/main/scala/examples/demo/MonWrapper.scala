25e39e0707a5059a9cdfe59b04bb3f647b8e5784
Christian Bartolo <christian.bartolo.16@um.edu.mt>
2021-02-18 10:05:44 +0100

    prep for video

--- examples/src/main/scala/examples/demo/MonWrapper.scala
+++ examples/src/main/scala/examples/demo/MonWrapper.scala
@@ -1,9 +1,9 @@
 package examples.demo
 
-import lchannels.{LocalChannel, SocketManager, SocketOut}
+import lchannels.{SocketManager, SocketOut}
 
 import java.io.{BufferedReader, BufferedWriter, InputStreamReader, OutputStreamWriter}
-import java.net.{ServerSocket, Socket}
+import java.net.Socket
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
@@ -16,28 +16,31 @@ object MonWrapper extends App {
     private val INCORRECTR = """INCORRECT""".r
     private val HINTR = """HINT (.*)""".r
     override def destreamer(): Any = inB.readLine() match {
-      case CORRECTR() => Correct()(SocketOut[ExternalChoice1](this));
-      case INCORRECTR() => Incorrect()(SocketOut[ExternalChoice1](this));
-      case HINTR(info) => Hint(info)(SocketOut[ExternalChoice1](this))
+//      case CORRECTR() => Correct()(SocketOut[ExternalChoice1](this));
+//      case INCORRECTR() => Incorrect()(SocketOut[ExternalChoice1](this));
+//      case HINTR(info) => Hint(info)(SocketOut[ExternalChoice1](this))
+      case _ =>
     }
 
     private val outB = new BufferedWriter(new OutputStreamWriter(out))
     override def streamer(x: Any): Unit = x match {
-      case Guess(num) => outB.write(f"GUESS $num\r\n"); outB.flush();
-      case Help() => outB.write(f"HELP\r\n"); outB.flush();
-      case Quit() => outB.write(f"QUIT\r\n"); outB.flush();
+//      case Guess(num) => outB.write(f"GUESS $num\r\n"); outB.flush();
+//      case Help() => outB.write(f"HELP\r\n"); outB.flush();
+//      case Quit() => outB.write(f"QUIT\r\n"); outB.flush();
+      case _ =>
     }
   }
-  println("[Mon] Connecting to 127.0.0.1 1337")
-  //  val serverSocket = new ServerSocket(1020)
-  //  val server = serverSocket.accept()
-  //args[0]
-  val conn = new Socket("127.0.0.1", 1337)
-  val monSktm = new MonSocketManager(conn)
-  val sChoice = SocketOut[ExternalChoice1](monSktm)
 
-  //args[1] check socat
-  val connectionManager = new GameConnectionManager(1330)
-  val Mon = new Mon(connectionManager, sChoice, 300, 4.4172)(global, timeout)
-  Mon.run()
+  val serverPort = args(1).toInt
+  println("[Mon] Connecting to 127.0.0.1 "+serverPort)
+
+  val serverConn = new Socket("127.0.0.1", serverPort)
+  val monSktm = new MonSocketManager(serverConn)
+//  val sChoice = SocketOut[ExternalChoice1](monSktm)
+
+  val clientPort = args(0).toInt
+  val zvalue = args(2).toFloat
+  val clientConnectionManager = new GameConnectionManager(clientPort)
+//  val Mon = new Mon(clientConnectionManager, sChoice, 300, zvalue)(global, timeout)
+//  Mon.run()
 }
