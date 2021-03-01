b7d2dcf748dee33ddf5fcf39c8e33255388db004
Christian Bartolo <christian.bartolo.16@um.edu.mt>
2021-03-01 12:12:05 +0100

    removed hidden files

--- examples/src/main/scala/examples/execute/pingpong/CPSPc.scala
+++
@@ -1,8 +0,0 @@
-package examples.execute.pingpong
-
-import lchannels.Out
-sealed abstract class ExternalChoice1
-case class Ping()(val cont: Out[Pong]) extends ExternalChoice1
-case class Pong()(val cont: Out[ExternalChoice1])
-case class Quit() extends ExternalChoice1
-case class MonStart()
