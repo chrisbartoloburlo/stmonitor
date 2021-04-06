f6e85d588c9a8e5b55c452bb65385e557d0ba455
Christian Bartolo <christian.bartolo.16@um.edu.mt>
2021-04-06 20:49:22 +0200

    removed benchmarks directory from repo

--- benchmarks/src/main/scala/benchmarks/akkhttppingpong/monitored/CPSPc.scala
+++
@@ -1,7 +0,0 @@
-package benchmarks.akkhttppingpong.monitored
-
-import lchannels.Out
-sealed abstract class ExternalChoice1
-case class Ping()(val cont: Out[Pong]) extends ExternalChoice1
-case class Pong()(val cont: Out[ExternalChoice1])
-case class Quit() extends ExternalChoice1
