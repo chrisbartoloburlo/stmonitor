45a50a794f817fe3e7dc67d481eb50192d5b9352
Christian Bartolo <christian.bartolo.16@um.edu.mt>
2021-04-08 21:06:14 +0200

    minor fix to script

--- scripts/smtp-benchmarks/monitored_experiment.sh
+++ scripts/smtp-benchmarks/monitored_experiment.sh
@@ -8,7 +8,7 @@ wd=`pwd`
 while [ "$experiments" -ne 0 ] ; do
   echo Running SMTP Monitored Experiment ${run} with ${iterations} emails
 
-  /usr/bin/time --format="%P,%M,%K" java -cp ./examples/target/scala-2.12/examples-assembly-0.0.3.jar examples.smtp.MonitoredClient $wd/results/monitored/ ${iterations} ${run} 1025 2>> $wd/scripts/smtp-benchmarks/results/monitored/${iterations}_cpu_mem_run.txt
+  /usr/bin/time --format="%P,%M,%K" java -cp ./examples/target/scala-2.12/examples-assembly-0.0.3.jar examples.smtp.MonitoredClient $wd/scripts/smtp-benchmarks/results/monitored/ ${iterations} ${run} 1025 2>> $wd/scripts/smtp-benchmarks/results/monitored/${iterations}_cpu_mem_run.txt
 
   sleep 1
 
