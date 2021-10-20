fcf98a3c8ba1c9179f5bfc2e17736aff23cd2819
Christian Bartolo <christian.bartolo.16@um.edu.mt>
2021-10-20 21:56:36 +0200

    bug fix in experiments

--- scripts/smtp-benchmarks/detached_monitored_experiment.sh
+++ scripts/smtp-benchmarks/detached_monitored_experiment.sh
@@ -16,11 +16,11 @@ while [ "$experiments" -ne 0 ] ; do
     port=25
   fi
 
-  screen -S stmonbench -dm bash -c "/usr/bin/time --format=\"%P,%M,%K\" java -cp ./examples/target/scala-2.12/examples-assembly-0.0.3.jar examples.smtp.MonWrapper $port 1026 2>> $wd/scripts/smtp-benchmarks/$type/results/detached_monitored/${iterations}_cpu_mem_run.txt"
+  screen -S stmonbench -dm bash -c "/usr/bin/time --format=\"%P,%M,%K\" java -cp ./examples/target/scala-2.12/examples-assembly-0.0.3.jar examples.smtp.MonWrapper $port 1026 2 $log>> $wd/scripts/smtp-benchmarks/$type/results/detached_monitored/${iterations}_cpu_mem_run.txt"
 
   sleep 1
 
-  java -cp ./examples/target/scala-2.12/examples-assembly-0.0.3.jar examples.smtp.Client $wd/scripts/smtp-benchmarks/$type/results/detached_monitored/ ${iterations} ${run} 1026 ${log}
+  java -cp ./examples/target/scala-2.12/examples-assembly-0.0.3.jar examples.smtp.Client $wd/scripts/smtp-benchmarks/$type/results/detached_monitored/ ${iterations} ${run} 1026
 
   screen -S stmonbench -p 0 -X stuff "^C" > /dev/null 2>&1
   echo "Finished SMTP $type Detached Monitored Experiment $run.\n"
