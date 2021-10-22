7a29153af76e52286aa3f55663294c9d9114cac6
Christian Bartolo <christian.bartolo.16@um.edu.mt>
2021-10-22 23:36:05 +0200

    fix bug in experiments

--- scripts/smtp-benchmarks/detached_monitored_experiment.sh
+++ scripts/smtp-benchmarks/detached_monitored_experiment.sh
@@ -8,8 +8,6 @@ run=1
 wd=`pwd`
 
 while [ "$experiments" -ne 0 ] ; do
-  echo "Running SMTP $type Detached Monitored Experiment $run with $iterations emails..."
-
   if [ "$type" = "smtp-python" ]; then
     port=1025
   else
@@ -17,8 +15,10 @@ while [ "$experiments" -ne 0 ] ; do
   fi
 
   if [ "$log" = "false" ]; then
+    echo "Running SMTP $type Detached Monitored without logging Experiment $run with $iterations emails..."
     screen -S stmonbench -dm bash -c "/usr/bin/time --format=\"%P,%M,%K\" java -cp ./examples/target/scala-2.12/examples-assembly-0.0.3.jar examples.smtp.MonWrapper $port 1026 $log 2>> $wd/scripts/smtp-benchmarks/$type/results/detached_monitored/${iterations}_cpu_mem_run.txt"
   elif [ "$log" = "true" ]; then 
+    echo "Running SMTP $type Detached Monitored with logging Experiment $run with $iterations emails..."
     screen -S stmonbench -dm bash -c "/usr/bin/time --format=\"%P,%M,%K\" java -cp ./examples/target/scala-2.12/examples-assembly-0.0.3.jar examples.smtp.MonWrapper $port 1026 $log 2>> $wd/scripts/smtp-benchmarks/$type/results/detached_monitored_logging/${iterations}_cpu_mem_run.txt"
   else
     echo "Error in logging flag"
--- scripts/smtp-benchmarks/monitored_experiment.sh
+++ scripts/smtp-benchmarks/monitored_experiment.sh
@@ -8,8 +8,6 @@ run=1
 wd=`pwd`
 
 while [ "$experiments" -ne 0 ] ; do
-  echo "Running SMTP $type Monitored Experiment $run with $iterations emails..."
-
   if [ "$type" = "smtp-python" ]; then
     port=1025
   else
@@ -17,9 +15,11 @@ while [ "$experiments" -ne 0 ] ; do
   fi
 
   if [ "$log" = "false" ]; then
-    /usr/bin/time --format="%P,%M,%K" java -cp ./examples/target/scala-2.12/examples-assembly-0.0.3.jar examples.smtp.MonitoredClient $wd/scripts/smtp-benchmarks/$type/results/monitored/ ${iterations} ${run} ${port} 2>> $wd/scripts/smtp-benchmarks/$type/results/monitored/${iterations}_cpu_mem_run.txt ${log}
-  elif [ "$log" = "true" ]; then 
-    /usr/bin/time --format="%P,%M,%K" java -cp ./examples/target/scala-2.12/examples-assembly-0.0.3.jar examples.smtp.MonitoredClient $wd/scripts/smtp-benchmarks/$type/results/monitored_logging/ ${iterations} ${run} ${port} 2>> $wd/scripts/smtp-benchmarks/$type/results/monitored_logging/${iterations}_cpu_mem_run.txt ${log}
+    echo "Running SMTP $type Monitored without logging Experiment $run with $iterations emails..."
+    /usr/bin/time --format="%P,%M,%K" java -cp ./examples/target/scala-2.12/examples-assembly-0.0.3.jar examples.smtp.MonitoredClient $wd/scripts/smtp-benchmarks/$type/results/monitored/ ${iterations} ${run} ${port} ${log} 2>> $wd/scripts/smtp-benchmarks/$type/results/monitored/${iterations}_cpu_mem_run.txt
+  elif [ "$log" = "true" ]; then
+    echo "Running SMTP $type Monitored with logging Experiment $run with $iterations emails..."
+    /usr/bin/time --format="%P,%M,%K" java -cp ./examples/target/scala-2.12/examples-assembly-0.0.3.jar examples.smtp.MonitoredClient $wd/scripts/smtp-benchmarks/$type/results/monitored_logging/ ${iterations} ${run} ${port} ${log} 2>> $wd/scripts/smtp-benchmarks/$type/results/monitored_logging/${iterations}_cpu_mem_run.txt
   else
     echo "Error in logging flag"
     exit
